c45022785e09af0be84876fe44d74e753512bbe7
jeffmanhart <jeffreymanhart@gmail.com>
2023-08-20 13:40:17 -0600

    Updates using JS to modify HTML

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById("main").remove();
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+newHeader.textContent = "Jeffrey is the champion";
+document.body.append(newHeader);
